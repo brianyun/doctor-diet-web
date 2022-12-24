a950542d3afc2603aa7cc7b864b89cffccffdb9f
huisang-growth <huisang.growth@gmail.com>
2022-12-24 13:00:39 +0900

    FEAT: 홈 화면 세팅

---
+++ pages/apply.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from 'common/palette';
+
+export const apply = () => {
+	return <Div>apply</Div>;
+};
+
+const Div = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+`;
